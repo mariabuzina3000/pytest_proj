c3e7891f4740659f3695deff0d95737cbd15de34
maria buzina <buzinanm@yandex.ru>
2023-06-11 13:08:27 +0500

    fixed tests_arrs

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -1,11 +1,11 @@
-from utils import arrs
-
+from pytest_proj.utils.arrs import get
+from pytest_proj.utils.arrs import my_slice
 
 def test_get():
-    assert arrs.get([1, 2, 3], 1, "test") == 3
-    assert arrs.get([], 0, "test") == "test"
+    assert get([1, 2, 3], 1, "test") == 2
+    assert get([], -1, "test") == "test"
 
 
-def test_slice():
-    assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
-    assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+def test_my_slice():
+    assert my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
+    assert my_slice([1, 2, 3], 1) == [2, 3]
